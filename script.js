238e21a544849bddd76937537912cdeb535ec2e4
Hadijka Maganbetova <hadijkamaganbetova@MacBook-Pro-Hadijka.local>
2023-02-11 00:00:16 +0600

    second

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 //     console.log("не верно")
 // }
 
-
+// 2)
 // let a = 50
 // let b = 100
 
@@ -17,7 +17,7 @@
 //     console.log( a < b)
 // }
 
-
+// 3)
 // let a =-2 
 
 // if(a > 0){
@@ -26,7 +26,7 @@
 //     console.log("negative")
 // }
 
-
+// 4)
 // let num = 45
 
 // if( num / 5 ){
@@ -35,7 +35,7 @@
 //     console.log("не четное")
 // }
 
-
+// 5)
 // let a = 10
 // let b = 2
 
@@ -45,7 +45,7 @@
 //     console.log("не четное")
 // }   
     
-
+// 6)
 //  let s = true
 
 // console.log(typeof s)
@@ -58,7 +58,7 @@
 
 //  console.log(typeof s)
 
-
+// 7)
 // let a = 15
 
 // if(a > 1 && a < 9){
@@ -67,7 +67,7 @@
 //     console.log("не верно")
 // }
 
-
+// 8)
 // let a = 10
 
 // if(a === 3 || a === 7 ){
@@ -76,7 +76,7 @@
 //     console.log(a / 10)
 // }
 
-
+// 9)
 // let a = 10
 
 // let b = 5
@@ -87,7 +87,7 @@
 //     console.log(a - b)
 // }
 
-
+// 10)
 // let a = 20
 
 // let b = 10
@@ -98,6 +98,71 @@
 //     console.log("не верно")
 // }
 
+// 11)
+// let month = 5
+
+// if(month >= 1 && month < 3){
+//     console.log("zima");
+// }else if(month >= 3 && month < 6){
+//     console.log("vesna");
+// }else if(month >= 6 && month < 9){
+//     console.log("leto");
+// }else if(month >= 9 && month < 12){
+//     console.log("osen");
+// }else{
+//     console.log("ne pravilno");
+// }
+
+// 12)
+// let day = 2
+// if(day >= 1 && day < 11){
+//     console.log("pervaya");
+// }else if(age>= 11 && day < 21){
+//     console.log("vtoraya");
+// }else if(day >= 21 && day < 31){
+//     console.log("tretya");
+// }else{
+//     console.log("nety");
+// }
+
+// 13)
+// let a = "12345"
+
+// if(a[0] === "1" || a[0] === "2" || a[0] === "3"){
+//     console.log("da");
+// }else{
+//     console.log("net");
+// }
+
+// 14)
+// let age = 14
+
+// if(age >= 10 && age < 21 ){
+//     console.log("Мне " + age + " лет");
+// }else if(age > 20 && age < 25){
+//     console.log("Мне " + age + " года");
+// }
 
 
+// let a = "12345"
 
+// if(a[a.length - 1] === "5"){
+//     console.log("da");
+// }else{
+//     console.log("net");
+// }
+
+// 15)
+// let time = 45
+
+// if(time = 1 && time <15){
+//     console.log("pervaya");
+// }else if(time >= 15 && time < 30){
+//     console.log("vtoraya");
+// }else if(time >= 30 && time < 45){
+//     console.log("tretya");
+// }else if(time >= 45 && time < 60){
+//     console.log("chetvertaya");
+// }else{
+//     console.log("nety");
+// }
