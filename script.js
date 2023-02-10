7aaadf74793200248f4a1a834fa4d74ec25bcff7
Hadijka Maganbetova <hadijkamaganbetova@MacBook-Pro-Hadijka.local>
2023-02-10 20:06:17 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,103 @@
+// 1)
+// let a = 10 
+
+// if (a == 10){
+//     console.log("верно")
+// }else{
+//     console.log("не верно")
+// }
+
+
+// let a = 50
+// let b = 100
+
+// if( a > b){
+//     console.log(a > b)
+// }else {
+//     console.log( a < b)
+// }
+
+
+// let a =-2 
+
+// if(a > 0){
+//     console.log("positive")
+// }else{
+//     console.log("negative")
+// }
+
+
+// let num = 45
+
+// if( num / 5 ){
+//     console.log("четное")
+// }else{
+//     console.log("не четное")
+// }
+
+
+// let a = 10
+// let b = 2
+
+// if( a % b == 0){
+//     console.log("четное")
+// }else{
+//     console.log("не четное")
+// }   
+    
+
+//  let s = true
+
+// console.log(typeof s)
+
+//  let s = 45
+
+//  console.log(typeof s)
+
+// let s = "hello"
+
+//  console.log(typeof s)
+
+
+// let a = 15
+
+// if(a > 1 && a < 9){
+//     console.log("верно")
+// }else{
+//     console.log("не верно")
+// }
+
+
+// let a = 10
+
+// if(a === 3 || a === 7 ){
+//     console.log(a + 7)
+// }else{
+//     console.log(a / 10)
+// }
+
+
+// let a = 10
+
+// let b = 5
+
+// if(a === 0 || a < 0 && b > 5 || b === 5){
+//     console.log(a + b)
+// }else{
+//     console.log(a - b)
+// }
+
+
+// let a = 20
+
+// let b = 10
+
+// if( a > 4 && a - 10 || b > 7 || b === 7 && b < 17){
+//     console.log("верно")
+// }else{
+//     console.log("не верно")
+// }
+
+
+
+
